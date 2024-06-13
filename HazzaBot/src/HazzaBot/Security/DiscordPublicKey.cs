93692faf907813b515e25aa4c95174441f855ad1
Harry Wyatt <36449385+harrywyatt5@users.noreply.github.com>
2024-06-13 01:32:06 +0100

    Started security

---
+++ HazzaBot/src/HazzaBot/Security/DiscordPublicKey.cs
@@ -0,0 +1,23 @@
+﻿namespace HazzaBot.Security;
+
+public class DiscordPublicKey
+{
+    internal Lazy<DiscordPublicKey> Singleton { get; } = new(() => new DiscordPublicKey());
+
+    internal byte[] PublicKey { get; }
+
+    private DiscordPublicKey()
+    {
+        // Attempt to get public key from environment variable
+        var publicKey = Environment.GetEnvironmentVariable("DISCORD_PUBLIC_KEY");
+        
+        if (string.IsNullOrEmpty(publicKey))
+        {
+            throw new InvalidOperationException("DISCORD_PUBLIC_KEY environment variable is not set.");
+        }
+        else
+        {
+            PublicKey = Convert.FromBase64String(publicKey);
+        }
+    }
+}
